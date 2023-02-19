8b851600f5afb0790584388e586af11badca1cdc
Jola0907 <115967036+Jola0907@users.noreply.github.com>
2023-02-19 11:43:51 +0100

    commit13

--- GOIT23/ZAJĘCIA4/script.js
+++ GOIT23/ZAJĘCIA4/script.js
@@ -1,5 +1,3 @@
-
-
 function myFunction() {
   const element = document.getElementById("id01");
   element.innerHTML = "New Heading";
@@ -25,26 +23,27 @@ przesylka.name = "List";
 przesylka2.name = "Paczka";
 
 
-function mojaFunkcja1() {
+function mojaFunkcja(id) {
 
   const element = document.getElementById("id01");
-  informacja = "Typ twojej przesylki to: " + przesylka.name;
-  element.innerHTML = informacja;
-
   const element2 = document.getElementById("id02");
-  informacja = "Przesyłka nie posiada wagi";
-  element2.innerHTML = informacja;
 
-}
+  if (id === 1) { //instrukcje dla przycisku z informacja o przesylce 1
 
-function mojaFunkcja2() {
+    informacja = "Typ twojej przesylki to: " + przesylka.name;
+    element.innerHTML = informacja;
+    
+    informacja = "Przesyłka nie posiada wagi";
+    element2.innerHTML = informacja;
+  }
 
-  const element = document.getElementById("id01");
-  informacja = "Typ twojej przesylki to: " + przesylka2.name;
-  element.innerHTML = informacja;
+  else if (id === 2) { //instrukcje dla przycisku z informacja o przesylce 2
 
-  const element2 = document.getElementById("id02");
-  informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
-  element2.innerHTML = informacja;
+    informacja = "Typ twojej przesylki to: " + przesylka2.name;
+    element.innerHTML = informacja;
+  
+    informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
+    element2.innerHTML = informacja;
+  }
 
 }
